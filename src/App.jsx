7103ac0d832c2bc6910c6d635b592595acf4c195
Caramel4764 <nina.dong4764@gmail.com>
2024-12-28 14:11:31 -0800

    fix: maxCard works properly now. Before, when card counts were high, the amount of card rendered was way less. No duplicate cards now.

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,59 @@ import Card from "./components/Card"
 function App() {
   const [highScore, setHighScore] = useState(0);
   const [score, setScore] = useState(0);
-  const [maxCard, setMaxCard] = useState(200);
+  //max card is 151
+  const [maxCard, setMaxCard] = useState(151);
+  //151
   const [cardList, setCardList] = useState([]);
   const pokeAPI = "https://pokeapi.co/api/v2/pokemon/";
+  function checkDupId(list, id) {
+    for (let j = 0; j < list.length; j++) {
+      if (list[j] == id) {
+        return true;
+      }
+    }
+    return false;
+  }
+  function addPokeCard(pokeIdList) {
+    let ranNum = Math.floor(Math.random()*151+1);
+    while (checkDupId(pokeIdList, ranNum)) {
+      ranNum = Math.floor(Math.random()*151+1);
+    }
+    pokeIdList.push(ranNum);
+  }
+  async function getPokeCard(pokeIdList, newCards) {
+    for (let i = 0; i<pokeIdList.length; i++) {
+    let pokeData = await fetch(pokeAPI+pokeIdList[i])
+      .then(response => response.json())
+      .then(data => {
+        let newCard = {
+          cardId: i,
+          hasClicked: false,
+          name: data.name.slice(0,1).toUpperCase()+data.name.slice(1, data.name.length),
+          src: data.sprites.front_default,
+          type: data.types[0].type.name, //first type listed
+        }
+        newCards.push(newCard);
+      })
+      .then(()=>{
+        if (newCards.length==pokeIdList.length) {
+          setCardList(newCards);
+        }
+      })
+      .catch(error => console.error('Error:', error));
+    }
+  }
   useEffect(()=>{
     let newCards = [];
-    let newCard;
-    for (let i = 0; i<maxCard; i++) {
-        let ranNum = Math.floor(Math.random()*151+1);
-        let pokeData = fetch(pokeAPI+ranNum)
-        .then(response => response.json())
-        .then(data => {
-          let isDup = false;
-          
-          newCard = {
-            name: data.name.slice(0,1).toUpperCase()+data.name.slice(1, data.name.length),
-            src: data.sprites.front_default,
-            type: data.types[0].type.name, //first type listed
-          }
-          for (let j = 0; j<newCards.length;j++) {
-            if (newCards[j].name == newCard.name) {
-              isDup = true;
-              i--;
-              break;
-            }
-          }
-          if (isDup == false) {
-            newCards.push(newCard);
-          }
-        })
-        .then(()=>{
-          setCardList(newCards);
-          console.log(newCards.length)
-        })
-        .catch(error => console.error('Error:', error));
-    }
+    let pokeIdList = [];
+    for (let i = 0; i<maxCard;i++) {
+      addPokeCard(pokeIdList);
+    } 
+    getPokeCard(pokeIdList, newCards);
+    /*for (let i = 0; i<maxCard; i++) {
+        
+        
+    }*/
   }, [])
   return (
     <>
